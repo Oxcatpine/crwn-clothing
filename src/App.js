5f7d6c9e5e246cab226a9bd990d324024fefa9db
Oxcatpine <fourwildcats.cz@gmail.com>
2021-07-30 16:19:35 +1000

    header

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ import {
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import shopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in/sign-in-and-sign-up.component';
 
 
 
 function App() {
   return (
     <div >
+     <Header/>
      
      <Switch>
       <Route exact path ='/' component = {HomePage}/>
       <Route  path ='/shop' component = {shopPage}/>
-      
-
-     </Switch>
+      <Route  path ='/signin' component = {SignInAndSignUpPage}/>
+      </Switch>
      
      
     </div>
